4e6d24253c12fa3105b28e888be74f0d2a3160ca
abcplusd <pavittar.singh@abcplusd.net>
2017-08-04 14:19:27 +0530

    youtube video converter

--- routes/youtube.js
+++ routes/youtube.js
@@ -10,14 +10,14 @@ const CONST = require('../api/constants/constants');
 
 const getStream = (url) => youtubeStream(url);
 
-router.post('/', function(req, res) {
+router.get('/', function(req, res) {
   // var filePath = path.join(__dirname, '..', 'video.mp4');
   // console.log(filePath);
 
   // const video =
   // console.log(req.body);
-  // ytdl(req.body.url).pipe(res);
-  getStream(req.body.url).pipe(res);
+  // getStream(req.query.url).pipe(res).on('error', () => console.log(arguments));
+  // res.send(req.query.url);
   // console.log(stream);
   // ffmpeg({source: stream}).audioBitrate(128).audioCodec('libmp3lame').toFormat('mp3').output(res).run();
   // console.log(ffmpegCommand);
@@ -31,6 +31,58 @@ router.post('/', function(req, res) {
   // proc.on('end', function() {
   //   console.log('finished');
   // });
+
+  // function errhandler(err) {
+  //   if (!err) {
+  //     console.log('conversion complete');
+  //     // res.send('success');
+  //   } else {
+  //     console.log('conversion failed');
+  //     // res.send('fail');
+  //   }
+  // }
+
+  // const fileConversion = (input) => {
+  //   ffmpeg(input).toFormat('mp3').on('end', function() {
+  //     console.log('conversion ended');
+  //     errhandler(null);
+  //   }).on('error', function(err) {
+  //     console.log('error: ', err.code, err.msg);
+  //     errhandler(err);
+  //   }).pipe(res, {end: true});
+  //   // .run();
+  // };
+
+  // fileConversion(ytdl(req.query.url));
+  // res.send('err');
+  // fileConversion(ytdl(req.query.url), res, function(err) {
+  //   if (!err) {
+  //     console.log('conversion complete');
+  //     // res.send('success');
+  //   } else {
+  //     console.log('conversion failed');
+  //     // res.send('fail');
+  //   }
+  // });
+  const audio = ytdl(req.query.url, {
+    filter: 'audioonly',
+    quality: '141'
+  });
+  ffmpeg(audio).audioCodec('libmp3lame').audioBitrate(128).toFormat('mp3').on('error', (err) => console.error(err)).on('end', () => console.log('Finished!')).pipe(res, {end: true});
+});
+
+router.get('/convert', (req, res) => {
+  console.log(path.join(__dirname, '..', 'routes', 'xyz.mp4'));
+  // res.send('path');
+  fileConversion(fs.createReadStream(path.join(__dirname, '..', 'routes', 'xyz.mp4')), './output.mp3', function(err) {
+    if (!err) {
+      console.log('conversion complete');
+      res.send('success');
+    } else {
+      console.log('conversion failed');
+      res.send('fail');
+    }
+  });
 });
 
 /* REMOVE THE KEY WHILE PUSHING INTO THE GIT */
